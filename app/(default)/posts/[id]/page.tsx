198daa00f2b78b251144fe4b00db258740759d4c
jothamardel <ardelmbiplang@gmail.com>
2024-06-19 02:55:53 +0100

    chore: updating application
    Signed-off-by: jothamardel <ardelmbiplang@gmail.com>

--- app/(default)/posts/[id]/page.tsx
+++ app/(default)/posts/[id]/page.tsx
@@ -1,3 +1,6 @@
+export default function SinglePost() {
+  return <div>Single page</div>;
+}
 // {/*
 // Note: This code includes an example of how to fetch data from an external JSON file that is hosted at https://raw.githubusercontent.com/cruip/cruip-dummy/main/job-board-posts.json. To facilitate this, we've included a lib directory in the root which contains a function that can fetch the JSON content. Additionally, we've defined the Post types in the types.d.ts file located in the root.
 // */}
